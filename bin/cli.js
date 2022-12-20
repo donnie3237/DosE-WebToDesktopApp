01f0e01d7d7db4312b5a77601b5038bc57a86055
SoRa <paradorn3237@gmail.com>
2022-12-20 14:40:30 +0700

    .

---
+++ bin/cli.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+ const {execSync} = require('child_process');
+ 
+ const runCommand = command => {
+    try {
+        execSync(`${command}`,{stdio:'inherit'});
+    } catch (e) {
+        console.error(`Failed to execute ${command}`,e)
+        return false ;
+    }
+    return true ;
+ }
+ const repoName = process.argv[2]
+ const gitCheckoutCommand = `git clone --depth 1 https://github.com/donnie3237/DosE-WebToDesktopApp ${repoName}`
+ const installDepsCommand =`cd ${repoName} && npm install`
+
+ console.log(`cloning with name ${repoName}`)
+
+  const checkOut = runCommand(gitCheckoutCommand);
+  if(!checkOut) process.exit(-1)
+
+  console.log(`installing ${repoName}`);
+  const installedDeps = runCommand(installDepsCommand);
+  if(!installedDeps) process.exit(-1)
+  
+  console.log('DOSE :)')
+  console.log('You are ready !!, follow this below to start ')
+  console.log(`cd ${repoName} && npm dose`)
